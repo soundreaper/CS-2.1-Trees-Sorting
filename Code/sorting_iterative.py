6e2ff07819ac86f69a78caec71b4acdabc9a5dfe
soundreaper <soundreaper@gmail.com>
2020-10-21 16:28:40 -0700

    complete implementing iterative sorting algorithms

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -6,6 +6,10 @@ def is_sorted(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
+    if items == sorted(items):
+        return True
+    else:
+        return False
 
 
 def bubble_sort(items):
@@ -15,6 +19,12 @@ def bubble_sort(items):
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Repeat until all items are in sorted order
     # TODO: Swap adjacent items that are out of order
+    n = len(items)
+
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if items[j] > items[j+1]:
+                items[j], items[j+1] = items[j+1], items[j]
 
 
 def selection_sort(items):
@@ -25,6 +35,15 @@ def selection_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
+    n = len(items)
+
+    for i in range(n):
+        min_element = i
+        for j in range(i+1, n):
+            if items[min_element] > items[j]:
+                min_element = j
+
+        items[i], items[min_element] = items[min_element], items[i]
 
 
 def insertion_sort(items):
@@ -35,3 +54,13 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+    n = len(items)
+
+    for i in range(1, n):
+        key = items[i]
+
+        j = i-1
+        while j >= 0 and key < items[j]:
+            items[j+1] = items[j]
+            j -= 1
+        items[j+1] = key
